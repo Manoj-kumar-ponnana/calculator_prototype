f4271004812ed407f55fa15402a10123720c1c45
Manoj Kumar ponnana <84012313+Manoj-kumar-ponnana@users.noreply.github.com>
2024-08-26 18:46:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,40 +3,40 @@ function add(){
     let b=document.getElementById('num2').value
     let res=document.getElementById('para')
     let sum=parseFloat(a)+parseFloat(b)
-    res.innerHTML=sum
+    res.innerHTML=sum.toFixed(2)
 }
 function sub(){
     let a=document.getElementById('num1').value
     let b=document.getElementById('num2').value
     let res=document.getElementById('para')
     let sub=parseFloat(a)-parseFloat(b)
-    res.innerHTML=sub
+    res.innerHTML=sub.toFixed(2)
 }
 function mul(){
     let a=document.getElementById('num1').value
     let b=document.getElementById('num2').value
     let res=document.getElementById('para')
     let mul=parseFloat(a)*parseFloat(b)
-    res.innerHTML=mul
+    res.innerHTML=mul.toFixed(2)
 }
 function div(){
     let a=document.getElementById('num1').value
     let b=document.getElementById('num2').value
     let res=document.getElementById('para')
     let div=parseFloat(a)/parseFloat(b)
-    res.innerHTML=div
+    res.innerHTML=div.toFixed(2)
 }
 function power(){
     let a=document.getElementById('num1').value
     let b=document.getElementById('num2').value
     let res=document.getElementById('para')
     let pow=parseFloat(a)**parseFloat(b)
-    res.innerHTML=pow
+    res.innerHTML=pow.toFixed(2)
 }
 function percent(){
     let a=document.getElementById('num1').value
     let b=document.getElementById('num2').value
     let res=document.getElementById('para')
     let per=(parseFloat(a)/parseFloat(b))*100
-    res.innerHTML=per +"%"
+    res.innerHTML=per.toFixed(2) +"%"
 }
