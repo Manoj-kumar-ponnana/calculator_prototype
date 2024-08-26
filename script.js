f728cad7f6e7cd4d7aa9818a9670c44f5b163153
Manoj Kumar ponnana <84012313+Manoj-kumar-ponnana@users.noreply.github.com>
2024-08-26 15:40:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function add(){
+    let a=document.getElementById('num1').value
+    let b=document.getElementById('num2').value
+    let res=document.getElementById('para')
+    let sum=parseFloat(a)+parseFloat(b)
+    res.innerHTML=sum
+}
+function sub(){
+    let a=document.getElementById('num1').value
+    let b=document.getElementById('num2').value
+    let res=document.getElementById('para')
+    let sub=parseFloat(a)-parseFloat(b)
+    res.innerHTML=sub
+}
+function mul(){
+    let a=document.getElementById('num1').value
+    let b=document.getElementById('num2').value
+    let res=document.getElementById('para')
+    let mul=parseFloat(a)*parseFloat(b)
+    res.innerHTML=mul
+}
+function div(){
+    let a=document.getElementById('num1').value
+    let b=document.getElementById('num2').value
+    let res=document.getElementById('para')
+    let div=parseFloat(a)/parseFloat(b)
+    res.innerHTML=div
+}
+function power(){
+    let a=document.getElementById('num1').value
+    let b=document.getElementById('num2').value
+    let res=document.getElementById('para')
+    let pow=parseFloat(a)**parseFloat(b)
+    res.innerHTML=pow
+}
+function percent(){
+    let a=document.getElementById('num1').value
+    let b=document.getElementById('num2').value
+    let res=document.getElementById('para')
+    let per=(parseFloat(a)/parseFloat(b))*100
+    res.innerHTML=per +"%"
+}
