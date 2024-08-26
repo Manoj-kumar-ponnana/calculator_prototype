f71f97144f256208e14b066a012ac194cac4a5e4
Manoj Kumar ponnana <84012313+Manoj-kumar-ponnana@users.noreply.github.com>
2024-08-26 21:43:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,40 +3,64 @@ function add(){
     let b=document.getElementById('num2').value
     let res=document.getElementById('para')
     let sum=parseFloat(a)+parseFloat(b)
-    res.innerHTML=sum.toFixed(2)
+    res.innerHTML=sum
 }
 function sub(){
     let a=document.getElementById('num1').value
     let b=document.getElementById('num2').value
     let res=document.getElementById('para')
     let sub=parseFloat(a)-parseFloat(b)
-    res.innerHTML=sub.toFixed(2)
+    res.innerHTML=sub
 }
 function mul(){
     let a=document.getElementById('num1').value
     let b=document.getElementById('num2').value
     let res=document.getElementById('para')
     let mul=parseFloat(a)*parseFloat(b)
-    res.innerHTML=mul.toFixed(2)
+    if(Math.trunc(mul)!=mul){
+        res.innerHTML=mul.toFixed(2)
+    }
+    else{
+        res.innerHTML=mul
+    }
+    // res.innerHTML=mul
 }
 function div(){
     let a=document.getElementById('num1').value
     let b=document.getElementById('num2').value
     let res=document.getElementById('para')
     let div=parseFloat(a)/parseFloat(b)
-    res.innerHTML=div.toFixed(2)
+    if(Math.trunc(div)!=div){
+        res.innerHTML=div.toFixed(2)
+    }
+    else{
+        res.innerHTML=div
+    }
+    // res.innerHTML=div.toFixed(2)
 }
 function power(){
     let a=document.getElementById('num1').value
     let b=document.getElementById('num2').value
     let res=document.getElementById('para')
     let pow=parseFloat(a)**parseFloat(b)
-    res.innerHTML=pow.toFixed(2)
+    if(Math.trunc(pow)!=pow){
+        res.innerHTML=pow.toFixed(2)
+    }
+    else{
+        res.innerHTML=pow
+    }
+    // res.innerHTML=pow.toFixed(2)
 }
 function percent(){
     let a=document.getElementById('num1').value
     let b=document.getElementById('num2').value
     let res=document.getElementById('para')
     let per=(parseFloat(a)/parseFloat(b))*100
-    res.innerHTML=per.toFixed(2) +"%"
+    if(Math.trunc(per)!=per){
+        res.innerHTML=per.toFixed(2)+"%"
+    }
+    else{
+        res.innerHTML=per +"%"
+    }
+    // res.innerHTML=per.toFixed(2) +"%"
 }
